34251a9f1a3b89c5088c4bed5f27cec05ec1c346
soverby <shane.overby@greatwide-tm.com>
2018-09-19 16:03:58 -0400

    Created ColorFinderController; ColorFinder/Index.cshtml; added Color Finder to navbar

---
+++ GitBranchingTestApplication/Controllers/ColorFinderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitBranchingTestApplication.Controllers
+{
+    public class ColorFinderController : Controller
+    {
+        // GET: ColorFinder
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
